aa23c3ca3a854c62e3bcfe5c033adc2e79dcb19c
hc <71583114+hctty27@users.noreply.github.com>
2026-09-07 18:43:12 +0800

    wire trusted proxy configuration

--- cmd/x-type-center/main.go
+++ cmd/x-type-center/main.go
@@ -86,7 +86,7 @@ func runServer(logger *slog.Logger, args []string) error {
 
 	registry := service.NewRegistry(db)
 	static := http.FileServer(http.FS(webassets.Files))
-	api := httpapi.New(registry, cfg.SkillPackagePath, cfg.PublicURL, logger)
+	api := httpapi.New(registry, cfg.SkillPackagePath, cfg.PublicURL, cfg.TrustedProxies, logger)
 	server := &http.Server{
 		Addr:         cfg.Addr,
 		Handler:      api.Routes(static),
@@ -245,5 +245,6 @@ Environment:
   TYPE_REGISTRY_DSN
   TYPE_REGISTRY_SKILL_PACKAGE_PATH
   TYPE_REGISTRY_PUBLIC_URL
+  TYPE_REGISTRY_TRUSTED_PROXIES
 `)
 }
